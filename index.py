982b0c1877303af7fa77c0f069f019cdb20d1745
JuanFlorez123 <juan.camilo.florez@correounivalle.edu.co>
2022-05-18 11:32:54 -0500

    Creacion ventana, label y botones

--- index.py
+++ index.py
@@ -1,4 +1,23 @@
 import numpy as np
+import tkinter as tk
+
+ventana = tk.Tk()
+ventana.geometry("400x300")
+ventana.title("Algoritmos de búsqueda")
+
+etiquetaSeleccion = tk.Label(ventana, text = "Seleccione el tipo de algoritmo de búsqueda a aplicar")
+etiquetaSeleccion.pack(fill = tk.X)
+etiquetaSeleccion.configure(font=("Arial", 12, "italic"))
+
+botonBNI = tk.Button(ventana, text = "NO INFORMADA", pady = 10, bg="blue")
+botonBNI.pack(fill = tk.X)
+botonBNI.configure(font=("Courier", 16, "italic"))
+botonBI = tk.Button(ventana, text = "INFORMADA", pady = 10, bg = "green")
+botonBI.pack(fill = tk.X)
+botonBI.configure(font=("Courier", 16, "italic"))
 
 A = np.loadtxt('matriz.txt',dtype=int)
 print(A)
+
+
+ventana.mainloop()
